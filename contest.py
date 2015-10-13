4bc90f86be33d5b87e28375300a625f25b828f8c
Greg Banks <gbanks@gmail.com>
2015-10-12 17:12:12 -0700

    add connection inc/dec signal handlers

--- contest.py
+++ contest.py
@@ -1,14 +1,16 @@
 """MongoDB connection test
 
-
 Usage:
     contest.py [options] [CONNECTION_STR]
 
 Opts:
     -h --help               print this help
     -v --verbose            log debug output
-    --rate RATE             rate at which to perform writes/reads (OPS/s) [default: .1]
+    -r --rate RATE          rate at which to perform writes/reads (OPS/s) [default: .1]
     -n --con-num NUM        the number of connections to start up [default: 50]
+    -s --step NUM           the number of connections to increase or decrease
+                            the pool by when receiving SIGUSER1 (-) or SIGUSER2 (+)
+                            [default: 50]
 
 Args:
     CONNECTION_STR          the connection string (a default hard-coded value
@@ -38,6 +40,8 @@ from pymongo import MongoClient
 
 _CONNECTION_STR = ''
 _DONE = False
+_INC_CLIENTS = False
+_DEC_CLIENTS = False
 _COL_BASE = 'foo'
 _COL_NUM = 10
 
@@ -75,9 +79,13 @@ def client(id_, uri, rate, stop_event):
 
 
 def sig_handler(num, frame):
+    global _DONE, _INC_CLIENTS, _DEC_CLIENTS
     if num == signal.SIGINT:
-        global _DONE
         _DONE = True
+    if num == signal.SIGUSR1:
+        _DEC_CLIENTS = True
+    if num == signal.SIGUSR2:
+        _INC_CLIENTS = True
 
 
 def main():
@@ -100,12 +108,16 @@ def main():
     c.close()
 
     signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGUSR1, sig_handler)
+    signal.signal(signal.SIGUSR2, sig_handler)
 
     stop_event = Event()
 
     logging.info('spawning clients...')
 
     clients = []
+    step = int(opts['--step'])
+
     try:
         for i in xrange(int(opts['--con-num'])):
             logging.debug('spawning client %d...', i)
@@ -113,6 +125,23 @@ def main():
                            opts['--rate'], stop_event))
 
         while not _DONE:
+            global _INC_CLIENTS, _DEC_CLIENTS
+            if _DEC_CLIENTS:
+                num_clients = len(clients)
+                for i in xrange(num_clients - 1,
+                                max(-1, num_clients - 1 - step),
+                                -1):
+                    logging.debug('killing client %d...', i)
+                    c = clients.pop()
+                    c.kill()
+                _DEC_CLIENTS = False
+            if _INC_CLIENTS:
+                num_clients = len(clients)
+                for i in xrange(step):
+                    logging.debug('spawning client %d...', num_clients + i)
+                    clients.append(gevent.spawn(client, i, _CONNECTION_STR,
+                                   opts['--rate'], stop_event))
+                _INC_CLIENTS = False
             gevent.sleep(.25)
 
         logging.info('signaling clients to exit...')
